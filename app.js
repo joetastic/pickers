552ba565d1613c7067ad4db780415569eafb0c85
jcarrafa <jcarrafa@etsy.com>
2012-05-18 20:00:50 -0400

    added auth

--- app.js
+++ app.js
@@ -4,16 +4,35 @@
  */
 
 var express = require('express')
-  , routes = require('./routes');
+  , routes = require('./routes')
+  , passport = require('passport');
 
 var app = module.exports = express.createServer();
+
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        return done(null, username);
+    }));
+
+passport.serializeUser(function(username, done) {
+    done(null, username);
+});
+
+passport.deserializeUser(function(username, done) {
+    done(null, username);
+});
 
 // Configuration
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+    app.use(express.cookieParser());
   app.use(express.bodyParser());
+    app.use(express.session({secret: 'caturday'}));
+    app.use(passport.initialize());
+    app.use(passport.session());
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
@@ -29,7 +48,25 @@ app.configure('production', function(){
 
 // Routes
 
-app.get('/', routes.index);
+app.get('/', ensureAuthenticated, routes.index);
+app.get('/login', function(req, res) {
+    res.render('signin', {title: 'Pickers'});
+});
+app.post('/login',
+         passport.authenticate('local', { successRedirect: '/',
+                                          failureRedirect: '/login',
+                                          failureFlash: true })
+        );
+
+app.get('/pick', ensureAuthenticated, routes.pick);
+app.post('/pick', ensureAuthenticated, routes.pickSubmit);
+app.get('/admin', ensureAuthenticated, routes.admin);
+app.post('/admin', ensureAuthenticated, routes.adminSubmit);
+
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) { return next(); }
+    res.redirect('/login')
+}
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,19 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' })
 };
+
+exports.pick = function(req, res){
+  res.render('pick', { title: 'Express' })
+};
+
+exports.pickSubmit = function(req, res){
+  res.render('pick', { title: 'Express' })
+};
+
+exports.admin = function(req, res){
+  res.render('admin', { title: 'Express' })
+};
+
+exports.adminSubmit = function(req, res){
+  res.render('admin', { title: 'Express' })
+};

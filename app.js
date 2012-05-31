26485797980c5a7d72f87f476323c055dd0bcf66
jcarrafa <jcarrafa@etsy.com>
2012-05-31 01:13:15 -0400

    added port env variable

--- app.js
+++ app.js
@@ -78,5 +78,5 @@ function ensureAuthenticated(req, res, next) {
     res.redirect('/login')
 }
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
